3d213ceb51b1cde55181b12fe81f476431d925cb
noob77777 <42897033+noob77777@users.noreply.github.com>
2022-07-17 12:53:32 +0530

    :add: web2.sql

---
+++ sql/web2.sql
@@ -0,0 +1,12 @@
+-- activity log
+CREATE TABLE activity_logs (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    img_url TEXT,
+    action TEXT,
+    value TEXT,
+    secondary_action TEXT,
+    secondary_value TEXT,
+    date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    category TEXT
+);
