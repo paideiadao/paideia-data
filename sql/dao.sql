07c043cd301ef1dccd6344784280293540b15777
noob77777 <42897033+noob77777@users.noreply.github.com>
2022-08-21 15:51:46 +0530

    :add: fk constraints for dao tables

--- sql/dao.sql
+++ sql/dao.sql
@@ -4,9 +4,9 @@ CREATE TABLE daos (
     dao_name TEXT NOT NULL,
     dao_short_description TEXT,
     dao_url TEXT NOT NULL,
-    governance_id INT,
-    tokenomics_id INT,
-    design_id INT,
+    governance_id INT, -- constraint maintained from governance table
+    tokenomics_id INT, -- constraint maintained from tokenomics table
+    design_id INT, -- constraint maintained from design table
     is_draft BOOLEAN,
     is_published BOOLEAN,
     nav_stage INT,
@@ -14,10 +14,12 @@ CREATE TABLE daos (
 );
 
 -- design
+CREATE TABLE dao_themes (id SERIAL PRIMARY KEY);
+
 CREATE TABLE dao_designs (
     id SERIAL PRIMARY KEY,
-    dao_id INT NOT NULL,
-    theme_id INT NOT NULL,
+    dao_id INT REFERENCES daos(id) ON DELETE CASCADE,
+    theme_id INT REFERENCES dao_themes(id),
     logo_url TEXT,
     show_banner BOOLEAN,
     banner_url TEXT,
@@ -25,11 +27,9 @@ CREATE TABLE dao_designs (
     footer_text TEXT
 );
 
-CREATE TABLE dao_themes (id SERIAL PRIMARY KEY);
-
 CREATE TABLE footer_social_links (
     id SERIAL PRIMARY KEY,
-    design_id INT NOT NULL,
+    design_id INT REFERENCES dao_designs(id) ON DELETE CASCADE,
     social_network TEXT,
     link_url TEXT
 );
@@ -37,7 +37,7 @@ CREATE TABLE footer_social_links (
 -- governance
 CREATE TABLE governances (
     id SERIAL PRIMARY KEY,
-    dao_id INT NOT NULL,
+    dao_id INT REFERENCES daos(id) ON DELETE CASCADE,
     is_optimistic BOOLEAN,
     is_quadratic_voting BOOLEAN,
     time_to_challenge__sec INT,
@@ -50,14 +50,14 @@ CREATE TABLE governances (
 
 CREATE TABLE governance_whitelist (
     id SERIAL PRIMARY KEY,
-    governance_id INT NOT NULL,
-    ergo_address_id INT NOT NULL
+    governance_id INT REFERENCES governances(id) ON DELETE CASCADE,
+    ergo_address_id INT REFERENCES ergo_addresses(id)
 );
 
 -- tokenomics and distributions
 CREATE TABLE tokenomics (
     id SERIAL PRIMARY KEY,
-    dao_id INT NOT NULL,
+    dao_id INT REFERENCES daos(id) ON DELETE CASCADE,
     type TEXT NOT NULL,
     token_name TEXT,
     token_ticker TEXT,
@@ -69,20 +69,20 @@ CREATE TABLE tokenomics (
 
 CREATE TABLE token_holders (
     id SERIAL PRIMARY KEY,
-    ergo_address_id INT NOT NULL,
+    ergo_address_id INT REFERENCES ergo_addresses(id),
     percentage DECIMAL,
     balance DECIMAL
 );
 
 CREATE TABLE tokenomics_token_holders (
     id SERIAL PRIMARY KEY,
-    token_holder_id INT NOT NULL,
-    tokenomics_id INT NOT NULL
+    token_holder_id INT REFERENCES token_holders(id) ON DELETE CASCADE,
+    tokenomics_id INT REFERENCES tokenomics(id) ON DELETE CASCADE
 );
 
 CREATE TABLE distributions (
     id SERIAL PRIMARY KEY,
-    tokenomics_id INT NOT NULL,
+    tokenomics_id INT REFERENCES tokenomics(id) ON DELETE CASCADE,
     distribution_type TEXT NOT NULL,
     balance DECIMAL,
     percentage DECIMAL
@@ -90,20 +90,20 @@ CREATE TABLE distributions (
 
 CREATE TABLE distribution_token_holders (
     id SERIAL PRIMARY KEY,
-    token_holder_id INT NOT NULL,
-    distribution_id INT NOT NULL
+    token_holder_id INT REFERENCES token_holders(id) ON DELETE CASCADE,
+    distribution_id INT REFERENCES distributions(id) ON DELETE CASCADE
 );
 
 CREATE TABLE airdrop_validated_fields (
     id SERIAL PRIMARY KEY,
-    distribution_id INT NOT NULL,
+    distribution_id INT REFERENCES distributions(id) ON DELETE CASCADE,
     value TEXT,
     number INT
 );
 
 CREATE TABLE distribution_config (
     id SERIAL PRIMARY KEY,
-    distribution_id INT NOT NULL,
+    distribution_id INT REFERENCES distributions(id) ON DELETE CASCADE,
     property_name TEXT,
     property_value TEXT,
     property_data_type TEXT DEFAULT 'str'
